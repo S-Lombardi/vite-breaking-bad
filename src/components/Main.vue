ce814dca431fbaa97640f647ab9fed31ac95008b
S-Lombardi <silo.lombardi@gmail.com>
2023-06-22 18:18:20 +0200

    Visualizzo a schermo i nomi dei Pokemonn con un ciclo for

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -10,13 +10,12 @@ export default {
 
         }
     },
-
     mounted(){
+        //chiamata Api filtrata
         axios.get('https://41tyokboji.execute-api.eu-central-1.amazonaws.com/dev/api/v1/pokemons?per=10').then((result) =>{
+            //inserisco nella variabile larray della chiamata con la lista dei Pokemon
             this.pokemonList = result.data.docs 
-        })
-        
-
+        })  
     }
     
 }
@@ -25,10 +24,13 @@ export default {
 
 
 <template lang="">
-    <main>
-        MAin
+    <main class="container">   
+        <div class="row justify-content-center align-items-center">
+            <div v-for="(pokemon, index) in pokemonList" :key="index"   class="col-6 col-md-4 col-lg-3">
+                {{ pokemon.name }}
+            </div>
+        </div>
     </main>
-    
 </template>
 
 
