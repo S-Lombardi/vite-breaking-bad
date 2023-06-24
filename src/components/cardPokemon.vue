ac4048acd51bba4c5fb773da670644bf9ee1a085
S-Lombardi <silo.lombardi@gmail.com>
2023-06-24 14:21:55 +0200

    Aggiunto colore di sfondo alle card  con una funzione che recupera i colori dall'array della lista dei pokemon

--- src/components/cardPokemon.vue
+++ src/components/cardPokemon.vue
@@ -3,13 +3,21 @@ export default {
     props:{
         myPokemon : Object,
     },
+
+    computed:{
+        //funzione per recuperare i colori delle card dall'array dei pokemon
+        getCardColor(){
+            return this.myPokemon.color.toLowerCase()
+        }
+
+    }
 }
 </script>
 
 
 <template lang="">
     
-    <div class="card d-flex flex-column align-items-center mt-3 mb-3 ">
+    <div class="card d-flex flex-column align-items-center mt-3 mb-3 color-filter" :style="{'background-color':getCardColor}">
         <img :src="myPokemon.imageUrl" class="mb-3 mt-3">
         <div class="">
             {{myPokemon.number}}
@@ -31,13 +39,14 @@ export default {
 <style lang="scss" scoped>
 @use '../styles/partials/variables' as *;
 
-.card {
-    background-color: rgb(151, 151, 151);
-}
+// .card {
+//     background-color: rgb(151, 151, 151);
+// }
 img{
     width: 120px;
     height: 120px;
     border-radius: 50%;
+    
 }
     
 </style>
